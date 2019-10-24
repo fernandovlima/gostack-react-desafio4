b8fba50cec3a14b0b5769db29bd6209809fec81a
fernandovlima <fernandovlima.s@gmail.com>
2019-10-24 11:22:27 -0300

    header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
 
+//components
+import Header from './components/Header';
+
 function App() {
-  return <div />;
+  return <Header />;
 }
 
 export default App;
